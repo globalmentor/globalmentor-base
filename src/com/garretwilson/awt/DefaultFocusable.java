180f986cc03e8de2f9a2783d01efe0157f1e73dd
Garret Wilson <garret@globalmentor.com>
2003-03-17 00:53:40 +0000

    implemented vCard name panel created vCard address class

--- src/com/garretwilson/awt/DefaultFocusable.java
+++ src/com/garretwilson/awt/DefaultFocusable.java
@@ -2,8 +2,8 @@ package com.garretwilson.awt;
 
 import java.awt.*;
 
-/**Indicates the the implementing class can indicate which component should
-	get the default focus. 
+/**Indicates the the implementing class can request the focus for its
+	default focus component. 
 @author Garret Wilson
 */
 public interface DefaultFocusable
@@ -14,5 +14,17 @@ public interface DefaultFocusable
 		unknown which component should get the default focus.
 	*/
 	public Component getDefaultFocusComponent();
+
+//TODO add a requestInitialFocus() method, and use that in OptionPane
+
+	/**Requests that the default focus component should get the default.
+	If the default focus comonent is itself <code>DefaultFocusable</code>, that
+		component is asked to request focus for its default focus component, and
+		so on.
+	@return <code>false</code> if the focus change request is guaranteed to
+		fail; <code>true</code> if it is likely to succeed.
+	@see Component#requestFocusInWindow
+	*/
+	public boolean requestDefaultFocusComponentFocus();
 	
 }
