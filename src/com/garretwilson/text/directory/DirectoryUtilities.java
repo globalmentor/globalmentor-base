7e192a6e266766afeaeac13f32522315e74d7f9f
Garret Wilson <garret@globalmentor.com>
2003-03-22 23:00:32 +0000

    added correct object creation for all currently used vCard types

--- src/com/garretwilson/text/directory/DirectoryUtilities.java
+++ src/com/garretwilson/text/directory/DirectoryUtilities.java
@@ -12,7 +12,7 @@ import com.garretwilson.util.*;
 public class DirectoryUtilities
 {
 	
-	/**Retrieves a parameter value of the content line.
+	/**Retrieves the first value of a parameter with the given name.
 	@param paramList The list of parameters, each item of which is a
 		<code>NameValuePair</code> with a name of type <code>String</code> and a
 		value of type <code>String</code>.
@@ -35,4 +35,36 @@ public class DirectoryUtilities
 		return null;	//show that we could not find a matching parameter
 	}
 
+	/**Retrieves the values of all parameters with the given name.
+	@param paramList The list of parameters, each item of which is a
+		<code>NameValuePair</code> with a name of type <code>String</code> and a
+		value of type <code>String</code>.
+	@param paramName The name of the parameter, which will be matched against
+		available parameters in a case insensitive way.
+	@return The values of all matching parameters. 
+	*/
+	public static String[] getParamValues(final List paramList, final String paramName)
+	{
+		final List paramValueList=new ArrayList(paramList);	//create a list to hold parameters, knowing we won't need room for more parameters than the we were given
+		final Iterator paramIterator=paramList.iterator();	//get an iterator to the parameters
+		while(paramIterator.hasNext())	//while there are more parameters
+		{
+			final NameValuePair parameter=(NameValuePair)paramIterator.next();	//get the next parameter name/value pair
+			if(paramName.equals(parameter.getName()))	//if this is the correct parameter
+			{
+				paramValueList.add((String)parameter.getValue());	//return the parameter value
+			}
+		}
+		return (String[])paramValueList.toArray(new String[paramValueList.size()]);	//return an array version of the list of values
+	}
+
+	/**An iterator to stop through all parameters with a given name.*/
+/*G***del; this is too complicated, and wouldn't save us from creating an object 
+	private static ParamIterator implements Iterator
+	{
+
+		
+		
+	}
+*/
 }
