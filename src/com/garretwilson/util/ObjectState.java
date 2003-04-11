06c697c1b6d8e158637c1eb8c72545658bf225d0
Garret Wilson <garret@globalmentor.com>
2003-04-11 21:18:20 +0000

    fixed Marmot menu, button, and action management across burrow views

--- src/com/garretwilson/util/ObjectState.java
+++ src/com/garretwilson/util/ObjectState.java
@@ -1,5 +1,7 @@
 package com.garretwilson.util;
 
+import java.beans.PropertyChangeListener;
+
 /**Provides state information of an existing object, such as:
 <ul>
 	<li>Descriptive properties of the object.</li>
@@ -41,5 +43,41 @@ public interface ObjectState extends Modifiable
 		property.
 	*/
 	public Object removeProperty(final Object key);
+
+	/**Adds a property change listener to the listener list.
+		The listener is registered for all properties.
+		<p>If the listener is <code>null</code>, no exception is thrown and no action
+		is performed.</p>
+	@param listener The <code>PropertyChangeListener</code> to be added.
+	@see PropertyChangeEvent
+	*/
+	public void addPropertyChangeListener(final PropertyChangeListener listener);
+
+	/**Remove a property change listener from the listener list.
+		This removes a <code>PropertyChangeListener</code> that was registered for
+		all properties.
+		<p>If the listener is <code>null</code>, no exception is thrown and no action
+		is performed.</p>
+	@param listener The <code>PropertyChangeListener</code> to be removed.
+	*/
+	public void removePropertyChangeListener(final PropertyChangeListener listener);
+
+	/**Add a property change listener for a specific property.
+		The listener will be invoked only when a call to
+		<code>firePropertyChange()</code> names that specific property.
+		<p>If the listener is <code>null</code>, no exception is thrown and no action
+		is performed.</p>
+	@param propertyName The name of the property to listen on.
+	@param listener The <code>PropertyChangeListener</code> to be added.
+	*/
+	public void addPropertyChangeListener(final String propertyName, final PropertyChangeListener listener);
+
+	/**Remove a property change listener for a specific property.
+		<p>If the listener is <code>null</code>, no exception is thrown and no
+		action is performed.</p>
+	@param propertyName The name of the property that was listened on.
+	@param listener The <code>PropertyChangeListener</code> to be removed
+	*/
+	public void removePropertyChangeListener(final String propertyName, final PropertyChangeListener listener);
 
 }
