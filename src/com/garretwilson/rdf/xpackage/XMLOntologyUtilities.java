cbd2dc12098066f9d1951ec952a5e63e489870c9
Garret Wilson <garret@globalmentor.com>
2005-03-04 21:10:39 +0000

    added support for default stylesheets for XML namespaces listed in an XPackage description

--- src/com/garretwilson/rdf/xpackage/XMLOntologyUtilities.java
+++ src/com/garretwilson/rdf/xpackage/XMLOntologyUtilities.java
@@ -21,4 +21,12 @@ public class XMLOntologyUtilities
 		return resource.getPropertyValueIterator(XML_ONTOLOGY_NAMESPACE_URI, STYLE_PROPERTY_NAME); //return an iterator to style properties
 	}
 
+	/**Retrieves an iterator to the XML namespace resources, represented by  <code>x:namespace</code> properties.
+	@param resource The resource the namespaces of which will be returned.
+	@return An iterator to the namespaces of the resource, if any.
+	*/
+	public static Iterator<RDFResource> getNamespaces(final RDFResource resource)
+	{
+		return resource.getPropertyValueIterator(XML_ONTOLOGY_NAMESPACE_URI, NAMESPACE_PROPERTY_NAME, RDFResource.class); //return an iterator to namespace properties
+	}
 }
