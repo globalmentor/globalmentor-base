e57268aa67bc78ba0cda213b28ab5f5837bff3b5
Garret Wilson <garret@globalmentor.com>
2005-06-28 12:52:27 +0000

    improved property binding support added property bindable interface

--- src/com/garretwilson/beans/AbstractPropertyValueChangeListener.java
+++ src/com/garretwilson/beans/AbstractPropertyValueChangeListener.java
@@ -18,16 +18,26 @@ public abstract class AbstractPropertyValueChangeListener<V> implements Property
 	@SuppressWarnings("unchecked")
 	public final void propertyChange(final PropertyChangeEvent propertyChangeEvent)
 	{
-		final PropertyValueChangeEvent<V> propertyValueChangeEvent;	//we'll create a property value change event of the correct generic type
+		propertyValueChange((PropertyValueChangeEvent<V>)getPropertyValueChangeEvent(propertyChangeEvent));	//call the generic version of the method with the genericized event object
+	}
+
+	/**Converts a property change event to a generics-aware property value change event.
+	This is a utility method to facilitate creation of property value change listeners that cannot subclass this class.
+	No checks are made at compile time to ensure the given event actually supports the given generic type.
+	@param propertyValueChangeEvent An event object describing the event source, the property that has changed, and its old and new values.
+	@return A generics-aware property change event, either cast from the provided object or created from the provided object's values as appropriate.
+	*/
+	@SuppressWarnings("unchecked")
+	public static <T> PropertyValueChangeEvent<T> getPropertyValueChangeEvent(final PropertyChangeEvent propertyChangeEvent)
+	{
 		if(propertyChangeEvent instanceof PropertyValueChangeEvent)	//if the event is a property value change event
 		{
-			propertyValueChangeEvent=(PropertyValueChangeEvent<V>)propertyChangeEvent;	//cast the event to a property value change event, assuming that it uses the correct generic type (if not, a class cast exception will be thrown later when the value is attempted to be used)
+			return (PropertyValueChangeEvent<T>)propertyChangeEvent;	//cast the event to a property value change event, assuming that it uses the correct generic type (if not, a class cast exception will be thrown later when the value is attempted to be used)
 		}
 		else	//if the event is a normal property change event
 		{
-			propertyValueChangeEvent=new PropertyValueChangeEvent<V>(propertyChangeEvent);	//create a copy of the event
+			return new PropertyValueChangeEvent<T>(propertyChangeEvent);	//create a copy of the event
 		}
-		propertyValueChange(propertyValueChangeEvent);	//call the generic version of the method with the genericized event object
 	}
 
 }
--- src/com/garretwilson/beans/BoundPropertyObject.java
+++ src/com/garretwilson/beans/BoundPropertyObject.java
@@ -2,57 +2,59 @@ package com.garretwilson.beans;
 
 import java.beans.*;
 
-/**An object that natively supports bound properties.
+/**An object that automatically supports bound properties.
 <p>Property change support is only created when needed; if no property change
 	listeners are added, the property change support will never be created or
 	invoked, even when firing property change events.</p>
-<p>This class was modeled from the property support of <code>java.awt.Component</code>.</p>
+<p>This class was modeled from the property support of {@link java.awt.Component}.</p>
 <p>This class creates generic-aware property value change events.</p>
 @author Garret Wilson
 @see PropertyChangeSupport
 @see PropertyValueChangeEvent
 @see PropertyValueChangeListener
 */
-public class BoundPropertyObject
+public class BoundPropertyObject implements PropertyBindable
 {
 
-	/**Keeps track of <code>PropertyChangeListener</code>s that have been
-		registered, and handles firing of events.
-	@see #addPropertyChangeListener
-	@see #removePropertyChangeListener
-	@see #firePropertyChange
-	*/
+	/**A convenience static empty array of property change listeners.*/
+	protected final static PropertyChangeListener[] NO_PROPERTY_CHANGE_LISTENERS=new PropertyChangeListener[0];
+
+	/**Lazily-created support that keeps track of <code>PropertyChangeListener</code>s that have been registered, and handles firing of events.*/
 	private PropertyChangeSupport propertyChangeSupport=null;
+
+		/**@return Support for property change event management, creating one if necessary.*/
+		protected synchronized PropertyChangeSupport getPropertyChangeSupport()
+		{
+			if(propertyChangeSupport==null)	//if we don't have property change support, yet
+			{
+				propertyChangeSupport=new PropertyChangeSupport(this);  //create new property change support				
+			}
+			return propertyChangeSupport;	//return the property change support
+		}
 
 	/**Default constructor.*/
 	public BoundPropertyObject()
 	{
 	}
 
-	/**Adds a property change listener to the listener list.
-		The listener is registered for all properties.
-		<p>If the listener is <code>null</code>, no exception is thrown and no action
-		is performed.</p>
+	/**Adds a property change listener to the listener list. The listener is registered for all properties.
+	<p>If the listener is <code>null</code>, no exception is thrown and no action is performed.</p>
 	@param listener The <code>PropertyChangeListener</code> to be added.
 	@see PropertyChangeEvent
 	*/
-	public synchronized void addPropertyChangeListener(final PropertyChangeListener listener)
+	public void addPropertyChangeListener(final PropertyChangeListener listener)
 	{
 		if(listener==null)  //if no listener was provided
 		  return; //don't do anything
-		if(propertyChangeSupport==null) //if no property change support has been created
-			propertyChangeSupport=new PropertyChangeSupport(this);  //create new property change support
-		propertyChangeSupport.addPropertyChangeListener(listener); //add the property change listener to our change support
+		getPropertyChangeSupport().addPropertyChangeListener(listener); //add the property change listener to our change support
 	}
 
 	/**Remove a property change listener from the listener list.
-		This removes a <code>PropertyChangeListener</code> that was registered for
-		all properties.
-		<p>If the listener is <code>null</code>, no exception is thrown and no action
-		is performed.</p>
+	This removes a <code>PropertyChangeListener</code> that was registered for all properties.
+	<p>If the listener is <code>null</code>, no exception is thrown and no action is performed.</p>
 	@param listener The <code>PropertyChangeListener</code> to be removed.
 	*/
-	public synchronized void removePropertyChangeListener(final PropertyChangeListener listener)
+	public void removePropertyChangeListener(final PropertyChangeListener listener)
 	{
 		if(listener==null)  //if no listener was provided
 		  return; //don't do anything
@@ -61,35 +63,54 @@ public class BoundPropertyObject
 	}
 
 	/**Add a property change listener for a specific property.
-		The listener will be invoked only when a call to
-		<code>firePropertyChange()</code> names that specific property.
-		<p>If the listener is <code>null</code>, no exception is thrown and no action
-		is performed.</p>
+	The listener will be invoked only when a call to {@link #firePropertyChange(String, V, V)} names that specific property.
+	<p>If the listener is <code>null</code>, no exception is thrown and no action is performed.</p>
 	@param propertyName The name of the property to listen on.
 	@param listener The <code>PropertyChangeListener</code> to be added.
 	*/
-	public synchronized void addPropertyChangeListener(final String propertyName, final PropertyChangeListener listener)
+	public void addPropertyChangeListener(final String propertyName, final PropertyChangeListener listener)
 	{
 		if(listener==null)  //if no listener was provided
 		  return; //don't do anything
-		if(propertyChangeSupport==null) //if no property change support has been created
-			propertyChangeSupport=new PropertyChangeSupport(this);  //create new property change support
-		propertyChangeSupport.addPropertyChangeListener(propertyName, listener); //add the property change listener to our change support, listening for the specified property
+		getPropertyChangeSupport().addPropertyChangeListener(propertyName, listener); //add the property change listener to our change support, listening for the specified property
 	}
 
 	/**Remove a property change listener for a specific property.
-		<p>If the listener is <code>null</code>, no exception is thrown and no
-		action is performed.</p>
+	<p>If the listener is <code>null</code>, no exception is thrown and no action is performed.</p>
 	@param propertyName The name of the property that was listened on.
 	@param listener The <code>PropertyChangeListener</code> to be removed
 	*/
-	public synchronized void removePropertyChangeListener(final String propertyName, final PropertyChangeListener listener)
+	public void removePropertyChangeListener(final String propertyName, final PropertyChangeListener listener)
 	{
 		if(listener==null)  //if no listener was provided
 		  return; //don't do anything
 		if(propertyChangeSupport!=null) //if we have property change support (if not, no listeners could have been added)
 			propertyChangeSupport.removePropertyChangeListener(propertyName, listener);  //remove the property change listener that was listening to a specified property from our change support
 	}
+
+  /**Returns an array of all the listeners that were added to the with {@link addPropertyChangeListener()}.
+	If some listeners have been added with a named property, then
+	the returned array will be a mixture of <code>PropertyChangeListener</code>s
+	and <code>PropertyChangeListenerProxy</code>s. If the calling
+	method is interested in distinguishing the listeners then it must
+	test each element to see if it's a <code>PropertyChangeListenerProxy</code>, perform the cast, and examine
+	the parameter.
+	@return all of the <code>PropertyChangeListener</code>s added or an empty array if no listeners have been added
+	@see PropertyChangeListenerProxy
+	*/
+	public PropertyChangeListener[] getPropertyChangeListeners()
+	{
+		return propertyChangeSupport!=null ? propertyChangeSupport.getPropertyChangeListeners() : NO_PROPERTY_CHANGE_LISTENERS;	//if we have property change support, delegate to that, else return an empty list
+	}
+
+	/**Returns an array of all the listeners which have been associated with the named property.
+	@return All of the <code>PropertyChangeListener</code>s associated with the named property;
+	if no such listeners have been added or if <code>propertyName</code> is <code>null</code>, an empty array is returned
+	*/
+	public PropertyChangeListener[] getPropertyChangeListeners(final String propertyName)
+	{
+		return propertyChangeSupport!=null ? propertyChangeSupport.getPropertyChangeListeners(propertyName) : NO_PROPERTY_CHANGE_LISTENERS;	//if we have property change support, delegate to that, else return an empty list
+  }
 
 	/**Reports that a bound property has changed. This method can be called
 		when a bound property has changed and it will send the appropriate
@@ -129,4 +150,5 @@ public class BoundPropertyObject
 			}
 		}
 	}
+
 }
