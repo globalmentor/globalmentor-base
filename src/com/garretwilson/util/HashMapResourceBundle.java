369f4457ab3f4e5baaf62a0fb001116b471e53cc
Garret Wilson <garret@globalmentor.com>
2005-10-05 23:43:03 +0000

    created map-backed resource bundle created resource bundle loading routine to allow XML-based resource bundles and resource bundle chaining integrated default resource bundles and XML resource bundles into Guise

---
+++ src/com/garretwilson/util/HashMapResourceBundle.java
@@ -0,0 +1,89 @@
+package com.garretwilson.util;
+
+import java.util.*;
+
+import static com.garretwilson.lang.ObjectUtilities.*;
+
+/**A resource bundle backed by a hash map.
+@author Garret Wilson
+@see java.util.PropertyResourceBundle
+*/
+public class HashMapResourceBundle extends ResourceBundle
+{
+
+	/**The map containing the resource mappings.*/
+	private final Map<String, Object> map;
+
+	/**Default constructor with no parent using a hash map.*/
+	public HashMapResourceBundle()
+	{
+		this((ResourceBundle)null);	//construct the class without a parent 
+	}
+
+	/**Parent constructor using a hash map.
+	@param parent The parent resource bundle, or <code>null</code> if there should be no parent for resolving resources.
+	*/
+	public HashMapResourceBundle(final ResourceBundle parent)
+	{
+		map=new HashMap<String, Object>();	//create a default hash map
+		setParent(parent);	//set the parent to that given
+	}
+
+	/**Map constructor with no parent.
+	Keys will be converted to strings.
+	@param map The map containing the resource mappings which will be copied to this map.
+	@exception NullPointerException if the given map is <code>null</code>.
+	*/
+	public HashMapResourceBundle(final Map<?, ?> map)
+	{
+		this(map, null);	//construct the class with no parent
+	}
+
+	/**Map and parent constructor.
+	Keys will be converted to strings.
+	@param map The map containing the resource mappings which will be copied to this map.
+	@param parent The parent resource bundle, or <code>null</code> if there should be no parent for resolving resources.
+	@exception NullPointerException if the given map is <code>null</code>.
+	*/
+	public HashMapResourceBundle(final Map<?, ?> map, final ResourceBundle parent)
+	{
+		this(parent);	//do the default construction
+		for(final Map.Entry<?, ?> entry:map.entrySet())	//for each entry in the map
+		{
+			final Object key=entry.getKey();	//get this entry's key
+			if(key!=null)	//if this entry has a key
+			{
+				this.map.put(key.toString(), entry.getValue());	//save this key and value in the map
+			}
+		}
+	}
+
+
+	/**Gets an object for the given key from this resource bundle.
+	@param key The key for the desired object.
+	@exception NullPointerException if the given key is <code>null</code>.
+	@return The object for the given key, or <code>null</code>.
+	*/
+	protected Object handleGetObject(final String key)
+	{
+		return map.get(checkNull(key, "Resource key cannot be null."));	//look up the object from the map
+	}
+
+	/**@return An enumeration of the resouce keys.*/
+	public Enumeration<String> getKeys()
+	{
+		final ResourceBundle parent=this.parent;	//get the parent resource bundle, if there is one
+		final Iterator<String> parentKeyIterator;	//get an iterator to the parent's keys, if there is a parent
+		if(parent!=null)	//if there is a parent
+		{
+			parentKeyIterator=new EnumerationIterator<String>(parent.getKeys());	//convert the parent key enumeration to an iterator
+		}
+		else	//if there is no parent
+		{
+			parentKeyIterator=null;	//there is no parent key iterator
+		}
+		final Iterator<String> keyIterator=map.keySet().iterator();	//get an iterator to the map's keys
+		return new JoinIterator<String>(keyIterator, parentKeyIterator);	//join our keys and the parent's keys
+	}
+}
+
---
+++ src/com/garretwilson/util/JoinIterator.java
@@ -0,0 +1,91 @@
+package com.garretwilson.util;
+
+import java.util.*;
+
+/**An iterator that joins the contents of two iterators.
+This class also implements the {@link Enumeration} interface.
+Element removal is not supported.
+@param <E> The type of element being iterated.
+@author Garret Wilson
+*/
+public class JoinIterator<E> implements Iterator<E>, Enumeration<E>	//TODO refactor to accept a vararg of iterators
+{
+	/**The first iterator, or <code>null</code> if the first iterator has been exhausted.*/
+	private Iterator<E> iterator1;
+
+	/**The second iterator, or <code>null</code> if there is no second iterator.*/
+	private Iterator<E> iterator2;
+
+	/**Iterator constructor.
+	@param iterator1 The first iterator, or <code>null</code> if there is no first iterator.
+	@param iterator2 The second iterator, containing the elements to use when the first iterator is exhausted, or <code>null</code> if there is no second iterator.
+	@exception NullPointerException if one of the given iterators is null.
+	*/
+	public JoinIterator(final Iterator<E> iterator1, final Iterator<E> iterator2)
+	{
+		this.iterator1=iterator1;
+		this.iterator2=iterator2;
+	}
+
+	/**@return <code>true</code> if the iteration has more elements.*/
+	public boolean hasNext()
+	{
+  	if(iterator1!=null)	//if we haven't exhausted the first iterator
+  	{
+  		if(iterator1.hasNext())	//if the iterator has another element
+  		{
+  			return true;	//show that we have another element
+  		}
+  		else	//if the first iterator has run out
+  		{
+  			iterator1=null;	//don't use the first iterator anymore
+  		}
+  	}
+ 		return iterator2!=null ? iterator2.hasNext() : null;	//see if the second iterator has more elements, if there is a second iterator
+	}
+
+	/**@return The next element in the iteration.
+	@exception NoSuchElementException if the iteration has no more elements.
+	*/
+	public E next()
+	{
+		if(hasNext())	//if we have elements (this will determine which iterator to use)
+		{
+			if(iterator1!=null)	//if there is a first iterator
+			{
+				return iterator1.next();	//get the iterator from the first iterator
+			}
+			else if(iterator2!=null)	//if there is a second iterator
+			{
+				return iterator2.next();	//get the iterator from the second iterator
+			}
+		}
+		throw new NoSuchElementException();
+	}
+
+	/**Removes from the underlying collection the last element returned by the iterator.
+	This implementation throws an exception, as removal is not supported.
+	@exception UnsupportedOperationException if the <code>remove</code> operation is not supported by this iterator.
+	*/
+	public void remove()
+	{
+		throw new UnsupportedOperationException("An enumeration iterator does not support removal.");
+	}
+
+	/**@return <code>true</code> if and only if this enumeration object contains at least one more element to provide; <code>false</code> otherwise.
+	This implementation delegates to {@link #hasNext()}.
+	*/
+  public boolean hasMoreElements()
+  {
+  	return hasNext();	//delegate to the iterator version
+  }
+
+  /**@return The next element of this enumeration.
+	This implementation delegates to {@link #next()}.
+	@exception  NoSuchElementException if no more elements exist.
+	*/
+	public E nextElement()
+	{
+		return next();	//delegate to the iterator version
+	}
+}
