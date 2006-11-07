37ac03e4835333efadd29c90e11f88c529e8cd85
Garret Wilson <garret@globalmentor.com>
2006-11-07 02:07:24 +0000

    Improved Guise ELFF log management. Added daily ELFF log file change.

---
+++ src/com/garretwilson/io/WriterDecorator.java
@@ -0,0 +1,208 @@
+package com.garretwilson.io;
+
+import java.io.*;
+
+import static com.garretwilson.lang.ObjectUtilities.checkInstance;
+
+/**Abstract class that wraps an existing writer.
+All versions of {@link Writer} methods delegate to the decorated writer.
+@author	Garret Wilson
+*/
+public abstract class WriterDecorator extends Writer
+{
+
+	/**The wrapped writer.*/
+	private final Writer writer;
+
+		/**@return The wrapped writer.*/
+		protected Writer getWriter() {return writer;}
+
+	/**Writer constructor with critical sections synchronized on the writer itself.
+	@param writer The writer being decorated.
+	@exception NullPointerException if the given writer is <code>null</code>.
+	*/
+	public WriterDecorator(final Writer writer)
+	{
+		super();	//construct the parent class using this class as a lock
+		this.writer=checkInstance(writer, "Writer cannot be null.");
+	}
+
+	/**Writer and lock constructor.
+	@param writer The writer being decorated.
+	@param lock The object to synchronize on for operations such as {@link #flush()} and {@link #close()}.
+	@exception NullPointerException if the given writer and/or lock is <code>null</code>.
+	*/
+	public WriterDecorator(final Writer writer, final Object lock)
+	{
+		super(lock);	//construct the parent class with the lock
+		this.writer=checkInstance(writer, "Writer cannot be null.");
+	}
+
+    /**
+     * Write a single character.  The character to be written is contained in
+     * the 16 low-order bits of the given integer value; the 16 high-order bits
+     * are ignored.
+     *
+     * <p> Subclasses that intend to support efficient single-character output
+     * should override this method.
+     *
+     * @param c  int specifying a character to be written.
+     * @exception  IOException  If an I/O error occurs
+     */
+    public void write(int c) throws IOException {getWriter().write(c);}
+
+    /**
+     * Write an array of characters.
+     *
+     * @param  cbuf  Array of characters to be written
+     *
+     * @exception  IOException  If an I/O error occurs
+     */
+    public void write(char cbuf[]) throws IOException {getWriter().write(cbuf);}
+
+    /**
+     * Write a portion of an array of characters.
+     *
+     * @param  cbuf  Array of characters
+     * @param  off   Offset from which to start writing characters
+     * @param  len   Number of characters to write
+     *
+     * @exception  IOException  If an I/O error occurs
+     */
+    public void write(char cbuf[], int off, int len) throws IOException {getWriter().write(cbuf, off, len);}
+
+    /**
+     * Write a string.
+     *
+     * @param  str  String to be written
+     *
+     * @exception  IOException  If an I/O error occurs
+     */
+    public void write(String str) throws IOException {getWriter().write(str);}
+
+    /**
+     * Write a portion of a string.
+     *
+     * @param  str  A String
+     * @param  off  Offset from which to start writing characters
+     * @param  len  Number of characters to write
+     *
+     * @exception  IOException  If an I/O error occurs
+     */
+    public void write(String str, int off, int len) throws IOException {getWriter().write(str, off, len);}
+
+    /**
+     * Appends the specified character sequence to this writer.
+     *
+     * <p> An invocation of this method of the form <tt>out.append(csq)</tt>
+     * behaves in exactly the same way as the invocation
+     *
+     * <pre>
+     *     out.write(csq.toString()) </pre>
+     *
+     * <p> Depending on the specification of <tt>toString</tt> for the
+     * character sequence <tt>csq</tt>, the entire sequence may not be
+     * appended. For instance, invoking the <tt>toString</tt> method of a
+     * character buffer will return a subsequence whose content depends upon
+     * the buffer's position and limit.
+     *
+     * @param  csq
+     *         The character sequence to append.  If <tt>csq</tt> is
+     *         <tt>null</tt>, then the four characters <tt>"null"</tt> are
+     *         appended to this writer.
+     *
+     * @return  This writer
+     *
+     * @throws  IOException
+     *          If an I/O error occurs
+     *
+     * @since  1.5
+     */
+    public Writer append(CharSequence csq) throws IOException {getWriter().append(csq); return this;}	//return this writer---not the underlying writer, which is what the decorated writer's version would return
+
+    /**
+     * Appends a subsequence of the specified character sequence to this writer.
+     * <tt>Appendable</tt>.
+     *
+     * <p> An invocation of this method of the form <tt>out.append(csq, start,
+     * end)</tt> when <tt>csq</tt> is not <tt>null</tt> behaves in exactly the
+     * same way as the invocation
+     *
+     * <pre>
+     *     out.write(csq.subSequence(start, end).toString()) </pre>
+     *
+     * @param  csq
+     *         The character sequence from which a subsequence will be
+     *         appended.  If <tt>csq</tt> is <tt>null</tt>, then characters
+     *         will be appended as if <tt>csq</tt> contained the four
+     *         characters <tt>"null"</tt>.
+     *
+     * @param  start
+     *         The index of the first character in the subsequence
+     *
+     * @param  end
+     *         The index of the character following the last character in the
+     *         subsequence
+     *
+     * @return  This writer
+     *
+     * @throws  IndexOutOfBoundsException
+     *          If <tt>start</tt> or <tt>end</tt> are negative, <tt>start</tt>
+     *          is greater than <tt>end</tt>, or <tt>end</tt> is greater than
+     *          <tt>csq.length()</tt>
+     *
+     * @throws  IOException
+     *          If an I/O error occurs
+     *
+     * @since  1.5
+     */
+    public Writer append(CharSequence csq, int start, int end) throws IOException {getWriter().append(csq, start, end); return this;}	//return this writer---not the underlying writer, which is what the decorated writer's version would return
+
+    /**
+     * Appends the specified character to this writer.
+     *
+     * <p> An invocation of this method of the form <tt>out.append(c)</tt>
+     * behaves in exactly the same way as the invocation
+     *
+     * <pre>
+     *     out.write(c) </pre>
+     *
+     * @param  c
+     *         The 16-bit character to append
+     *
+     * @return  This writer
+     *
+     * @throws  IOException
+     *          If an I/O error occurs
+     *
+     * @since 1.5
+     */
+    public Writer append(char c) throws IOException {getWriter().append(c); return this;}	//return this writer---not the underlying writer, which is what the decorated writer's version would return
+
+    /**
+     * Flush the stream.  If the stream has saved any characters from the
+     * various write() methods in a buffer, write them immediately to their
+     * intended destination.  Then, if that destination is another character or
+     * byte stream, flush it.  Thus one flush() invocation will flush all the
+     * buffers in a chain of Writers and OutputStreams.
+     * <p>
+     * If the intended destination of this stream is an abstraction provided by
+     * the underlying operating system, for example a file, then flushing the
+     * stream guarantees only that bytes previously written to the stream are
+     * passed to the operating system for writing; it does not guarantee that
+     * they are actually written to a physical device such as a disk drive.
+     *
+     * @exception  IOException  If an I/O error occurs
+     */
+    public void flush() throws IOException {getWriter().flush();}
+
+    /**
+     * Close the stream, flushing it first.  Once a stream has been closed,
+     * further write() or flush() invocations will cause an IOException to be
+     * thrown.  Closing a previously-closed stream, however, has no effect.
+     *
+     * @exception  IOException  If an I/O error occurs
+     */
+    public void close() throws IOException {getWriter().close();}
+
+}
