054ddadc6c26db69236a5a3e43b0c1ffcd943f34
Garret Wilson <garret@globalmentor.com>
2007-02-15 21:12:38 +0000

    Improved HTTP and WebDAV support, putting in place new classes for improving WebDAV property support and implementing WebDAV PROPPATCH.

---
+++ src/com/garretwilson/util/ObjectDecorator.java
@@ -0,0 +1,50 @@
+package com.garretwilson.util;
+
+import static com.garretwilson.lang.ObjectUtilities.checkInstance;
+
+/**An object that decorates another object,
+	preserving the {@link Object#hashCode()} and {@link Object#equals(Object)} of the decorated object.
+Equality is only supported for exact top-level types.
+@param <T> The type of object being decorated.
+@author Garret Wilson
+*/
+public class ObjectDecorator<T>
+{
+
+	/**The decorated object.*/
+	private final T decoratedObject;
+
+		/**@return The decorated object.*/
+		protected T getDecoratedObject() {return decoratedObject;}
+
+	/**Decorated object constructor.
+	@param decoratedObject The object to decorate.
+	@exception NullPointerException if the given object is <code>null</code>.
+	*/
+	public ObjectDecorator(final T decoratedObject)
+	{
+		this.decoratedObject=checkInstance(decoratedObject, "Decorated object cannot be null.");
+	}
+
+	/**Returns the hash code of this object.
+	This version returns the hash code of the decorated object.
+	@return The hash code of this object.
+	*/
+	public int hashCode()
+	{
+		return getDecoratedObject().hashCode();	//return the hash code of the decorated object
+	}
+
+	/**Determines if this object equals another object.
+	This version considers the given object equal to this object if it is of the same type
+		as this object and this object's decorated object's {@link Object#equals(Object)} method also returns <code>true</code>
+		for the objects's decorated object.
+	@param object The object to compare with this object.
+	@return <code>true</code> if the given object is considered equal to this object.
+	*/
+	public boolean equals(final Object object)
+	{
+		return getClass().isInstance(object) && getDecoratedObject().equals(((ObjectDecorator)object).getDecoratedObject());	//see if the object is of this class and our decorated object is equal to its decorated object
+	}
+	
+}
