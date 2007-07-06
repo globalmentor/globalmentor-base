6840c029600fc4462b0a38593a153c4b9fb44dfc
Garret Wilson <garret@globalmentor.com>
2007-07-06 01:24:26 +0000

    Added Marmox resource download functionality.

--- src/com/garretwilson/net/mime/ContentDispositionType.java
+++ src/com/garretwilson/net/mime/ContentDispositionType.java
@@ -5,16 +5,16 @@ package com.garretwilson.net.mime;
 	"Communicating Presentation Information in Internet Messages: The Content-Disposition Header".
 @author Garret Wilson
 @see javax.mail.internet.ContentDisposition
-@see http://www.rfc-editor.org/rfc/rfc1806.txt
+@see <a href="http://www.rfc-editor.org/rfc/rfc1806.txt">Communicating Presentation Information in Internet Messages - The Content-Disposition Header</a>
 @author Garret Wilson
 */
 public enum ContentDispositionType
 {
 
 	/**Indicates a bodypart is intended to be displayed automatically upon display of the message.*/	
-	inline,
+	INLINE,
 
 	/**Indicates a bodypart is separate from the main body of the mail message.*/	
-	attachment
+	ATTACHMENT;
 
 }
