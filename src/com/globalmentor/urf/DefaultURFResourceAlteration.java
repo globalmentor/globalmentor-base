aa6bf3ac8f2a20055e1791e4173d23adec69a7c1
Garret Wilson <garret@globalmentor.com>
2008-04-26 17:05:29 +0000

    Improved Marmot WebDAV repository property support.

--- src/com/globalmentor/urf/DefaultURFResourceAlteration.java
+++ src/com/globalmentor/urf/DefaultURFResourceAlteration.java
@@ -3,6 +3,7 @@ package com.globalmentor.urf;
 import java.net.URI;
 import java.util.*;
 
+import static java.util.Arrays.*;
 import static java.util.Collections.*;
 
 import static com.globalmentor.java.Objects.*;
@@ -54,12 +55,10 @@ public class DefaultURFResourceAlteration implements URFResourceAlteration
 		public Set<URFProperty> getPropertyAdditions() {return propertyAdditions;} 
 
 	/**Default constructor with no alterations.*/
+	@SuppressWarnings("unchecked")
 	DefaultURFResourceAlteration()
 	{
-		this.resourceURI=null;
-		this.propertyURIRemovals=emptySet();
-		this.propertyRemovals=emptySet();
-		this.propertyAdditions=emptySet();
+		this(null, (Set<URI>)EMPTY_SET, (Set<URFProperty>)EMPTY_SET, (Set<URFProperty>)EMPTY_SET);
 	}
 
 	/**Property URI removals, property removals, and property additions constructor.
@@ -83,10 +82,58 @@ public class DefaultURFResourceAlteration implements URFResourceAlteration
 	*/
 	public DefaultURFResourceAlteration(final URI resourceURI, final Collection<URI> propertyURIRemovals, final Collection<URFProperty> propertyRemovals, final Collection<URFProperty> propertyAdditions)
 	{
+		this(resourceURI,
+				unmodifiableSet(new HashSet<URI>(propertyURIRemovals)),
+				unmodifiableSet(new HashSet<URFProperty>(propertyRemovals)),
+				unmodifiableSet(new HashSet<URFProperty>(propertyAdditions)));
+	}
+
+	/**Resource URI, property URI removals, property removals, and property additions constructor.
+	This constructor saves the given collections as-is, so only private unmodifiable sets should be provided
+	@param resourceURI The new URI of the resource, or <code>null</code> if the current URI of the resource, if any, should remain unchanged.
+	@param propertyURIRemovals The URIs of properties to remove.
+	@param propertyRemovals The properties and values to remove.
+	@param propertyAdditions The properties and values to add.
+	@throws NullPointerException if the given property URI removals, property removals, and/or property additions is <code>null</code>.
+	*/
+	protected DefaultURFResourceAlteration(final URI resourceURI, final Set<URI> propertyURIRemovals, final Set<URFProperty> propertyRemovals, final Set<URFProperty> propertyAdditions)
+	{
 		this.resourceURI=resourceURI;
-		this.propertyURIRemovals=unmodifiableSet(new HashSet<URI>(checkInstance(propertyURIRemovals, "Property URI removals cannot be null")));
-		this.propertyRemovals=unmodifiableSet(new HashSet<URFProperty>(checkInstance(propertyRemovals, "Property removals cannot be null")));
-		this.propertyAdditions=unmodifiableSet(new HashSet<URFProperty>(checkInstance(propertyAdditions, "Property additions cannot be null")));
+		this.propertyURIRemovals=checkInstance(propertyURIRemovals, "Property URI removals cannot be null");
+		this.propertyRemovals=checkInstance(propertyRemovals, "Property removals cannot be null");
+		this.propertyAdditions=checkInstance(propertyAdditions, "Property additions cannot be null");
+	}
+
+	/**Creates an alteration to set the given properties by removing all properties with the URIs of the properties to set
+	and then adding the set properties.
+	No resource URI change is indicated.
+	@param properties The properties to set.
+	@return A resource alteration indicating that the given properties should be set.
+	@throws NullPointerException if the given properties is <code>null</code>.
+	*/
+	@SuppressWarnings("unchecked")
+	public static DefaultURFResourceAlteration createSetPropertiesAlteration(final URFProperty... properties)
+	{
+		final Set<URI> propertyURIRemovals=new HashSet<URI>();	//the URIs of the properties to remove
+		final Set<URFProperty> propertyAdditions=new HashSet<URFProperty>();	//the properties to add
+		for(final URFProperty property:properties)	//for each property
+		{
+			propertyURIRemovals.add(property.getPropertyURI());	//indicate that we should remove all values of this property
+			propertyAdditions.add(property);	//indicate that we should add this property
+		}
+		return new DefaultURFResourceAlteration(null, unmodifiableSet(propertyURIRemovals), (Set<URFProperty>)EMPTY_SET, unmodifiableSet(propertyAdditions));
+	}
+
+	/**Creates an alteration to remove all values of the properties with the givin URIs.
+	No resource URI change is indicated.
+	@param propertyURIRemovals The URIs of properties to remove.
+	@return A resource alteration indicating that the given properties should be removed.
+	@throws NullPointerException if the given properties is <code>null</code>.
+	*/
+	@SuppressWarnings("unchecked")
+	public static DefaultURFResourceAlteration createRemovePropertiesAlteration(final URI... propertyURIRemovals)
+	{
+		return new DefaultURFResourceAlteration(null, unmodifiableSet(new HashSet<URI>(asList(propertyURIRemovals))), (Set<URFProperty>)EMPTY_SET, (Set<URFProperty>)EMPTY_SET);
 	}
 
 	/**Combines the given alterations with the alterations specified in this object and returns a new specification of the union of alterations.
@@ -104,7 +151,6 @@ public class DefaultURFResourceAlteration implements URFResourceAlteration
 		final Set<URFProperty> propertyAdditions=new HashSet<URFProperty>(getPropertyAdditions());	//get the properties to add
 		propertyAdditions.addAll(resourceAlteration.getPropertyAdditions());	//update the properties to add
 		return new DefaultURFResourceAlteration(resourceAlterationResourceURI!=null ? resourceAlterationResourceURI : getResourceURI(), propertyURIRemovals, propertyRemovals, propertyAdditions);	//return a new resource alteration, overriding the specified resource URI if requested
-
 	}
 
 }
