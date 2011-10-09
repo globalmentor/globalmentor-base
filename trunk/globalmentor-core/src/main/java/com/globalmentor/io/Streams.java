bb90756bdba2c0c0e2f22f39856f94f27a63bb5f
Garret Wilson <garret@globalmentor.com>
2011-10-09 02:05:22 +0000

    Added more efficient stream copying with progress updates.

--- trunk/globalmentor-core/src/main/java/com/globalmentor/io/Streams.java
+++ trunk/globalmentor-core/src/main/java/com/globalmentor/io/Streams.java
@@ -18,11 +18,8 @@ package com.globalmentor.io;
 
 import java.io.*;
 
-import static java.lang.System.*;
-import java.util.*;
-
-import com.globalmentor.java.Bytes;
-import com.globalmentor.text.CharacterEncoding;
+import com.globalmentor.event.ProgressEvent;
+import com.globalmentor.event.ProgressListener;
 
 /**
  * Utilities for working with streams generally.
@@ -58,14 +55,63 @@ public class Streams
 	 */
 	public static long copy(final InputStream inputStream, final OutputStream outputStream, final long expectedContentLength) throws IOException
 	{
-		final int bufferSize = expectedContentLength >= 16 * 1024 * 1024 ? 10 * 1024 * 1024 : 64 * 1024; //create a buffer of the correct size, based upon the expected length if known; use a 10MB buffer for anything at least 10MB, or a 16KB buffer for everything else
+		return copy(inputStream, outputStream, expectedContentLength, null); //we don't have a progress listener
+	}
+
+	/**
+	 * Copies all information from an input stream to an output stream. Both streams are used as-is. If buffered reading and writing is desired, the streams
+	 * should be wrapped in a {@link BufferedInputStream} and a {@link BufferedOutputStream} and those should be passed as parameters. After copying is finished,
+	 * both streams are left open.
+	 * @param inputStream The source of the data.
+	 * @param outputStream The destination of the data.
+	 * @param expectedContentLength The length of content expected, or -1 if the length is unknown.
+	 * @param progressListener A listener to be notified of progress, or <code>null</code> if no progress notifications is requested.
+	 * @return The total number of bytes copied.
+	 * @throws IOException Thrown if there is an error reading from or writing to a stream.
+	 * @throws IOException if an expected content length was given and the number of bytes written to the output stream is not what was expected.
+	 */
+	public static long copy(final InputStream inputStream, final OutputStream outputStream, final long expectedContentLength,
+			final ProgressListener progressListener) throws IOException
+	{
+		final int bufferSize; //determine the optimal buffer size to use
+		/*TODO check the amount of memory before trying to use such large buffers
+		if(expectedContentLength >= 1 * 1024 * 1024 * 1024) //if the expected content length is over 1GB
+		{
+			bufferSize = 100 * 1024 * 1024; //use a 100MB buffer
+		}
+		else if(expectedContentLength >= 64 * 1024 * 1024) //if the expected content length is over 64MB
+		{
+			bufferSize = 50 * 1024 * 1024; //use a 50MB buffer
+		}
+		*/
+		if(expectedContentLength >= 16 * 1024 * 1024) //if the expected content length is over 16MB
+		{
+			bufferSize = 10 * 1024 * 1024; //use a 10MB buffer
+		}
+		else if(expectedContentLength >= 1 * 1024 * 1024) //if the expected content length is over 1MB
+		{
+			bufferSize = 1 * 1024 * 1024; //use a 1MB buffer
+		}
+		else
+		//for smaller (or unknown) sizes
+		{
+			bufferSize = 64 * 1024; //use a 16KB buffer for everything else
+		}
 		final byte[] buffer = new byte[bufferSize]; //create a buffer for copying data
 		long totalBytesCopied = 0; //show that we have not copied any data
 		int bytesRead; //this will store the number of bytes read each time
+		if(progressListener != null) //if we have a progress listener
+		{
+			progressListener.progressed(new ProgressEvent(Streams.class, bufferSize, 0, expectedContentLength)); //indicate that we are starting but have made no progress
+		}
 		while((bytesRead = inputStream.read(buffer)) >= 0) //read bytes until the end of the input stream is reached
 		{
 			outputStream.write(buffer, 0, bytesRead); //write the bytes to the output stream
 			totalBytesCopied += bytesRead; //update the total bytes read
+			if(progressListener != null) //if we have a progress listener
+			{
+				progressListener.progressed(new ProgressEvent(Streams.class, bytesRead, totalBytesCopied, expectedContentLength)); //indicate our progress
+			}
 		}
 		if(expectedContentLength >= 0 && totalBytesCopied != expectedContentLength) //if we didn't copy what was expected
 		{
