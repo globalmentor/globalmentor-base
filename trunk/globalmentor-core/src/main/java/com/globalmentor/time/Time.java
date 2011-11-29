c322e162c647e0fc47ae6f4d92c49ac83f619789
Garret Wilson <garret@globalmentor.com>
2011-11-29 20:25:49 +0000

    Implemented time-related convenience methods.

---
+++ trunk/globalmentor-core/src/main/java/com/globalmentor/time/Time.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2011 GlobalMentor, Inc. <http://www.globalmentor.com/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.globalmentor.time;
+
+/**
+ * Constants and methods for working with time in general, without regard to leap years, time zones, and the like.
+ * 
+ * @author Garret Wilson
+ */
+public class Time
+{
+
+	/** The number of milliseconds in a second. */
+	public static int MILLISECONDS_PER_SECOND = 1000;
+
+	/** The number of seconds in a minute. */
+	public static int SECONDS_PER_MINUTE = 60;
+
+	/** The number of minutes in an hour. */
+	public static int MINUTES_PER_HOUR = 60;
+
+	/** The number of hours in a day. */
+	public static int HOURS_PER_DAY = 24;
+}
---
+++ trunk/globalmentor-core/src/test/java/com/globalmentor/time/MillisecondsTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright © 1996-2011 GlobalMentor, Inc. <http://www.globalmentor.com/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.globalmentor.time;
+
+import static junit.framework.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Tests for millisecond processing.
+ * 
+ * @author Garret Wilson
+ */
+public class MillisecondsTest
+{
+
+	@Test
+	public void testSeconds()
+	{
+		assertEquals(5 * 1000, Milliseconds.fromSeconds(5));
+	}
+
+	@Test
+	public void testMinutes()
+	{
+		assertEquals(5 * 60 * 1000, Milliseconds.fromMinutes(5));
+	}
+
+	@Test
+	public void testHours()
+	{
+		assertEquals(5 * 60 * 60 * 1000, Milliseconds.fromHours(5));
+	}
+
+	@Test
+	public void testDays()
+	{
+		assertEquals(5 * 24 * 60 * 60 * 1000, Milliseconds.fromDays(5));
+	}
+
+}
