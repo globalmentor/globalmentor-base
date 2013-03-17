be0a9004203c459cca3b852e28627ea4c3aed079
Garret Wilson <garret@globalmentor.com>
2013-03-17 15:30:31 +0000

    PLOOP-9: Implemented session resource lookup by type.

---
+++ trunk/globalmentor-core/src/main/java/com/globalmentor/net/AbstractObjectResource.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright © 2013 GlobalMentor, Inc. <http://www.globalmentor.com/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.globalmentor.net;
+
+import static com.globalmentor.java.Objects.*;
+
+import java.net.URI;
+
+/**
+ * The abstract base class for a Java object that is also a resource.
+ * <p>
+ * This implementation considers objects equal if they are of the the specified runtime type and have the same URI.
+ * </p>
+ * @author Garret Wilson
+ */
+public abstract class AbstractObjectResource<C extends AbstractObjectResource<C>> extends DefaultResource implements Comparable<Resource>
+{
+
+	/**
+	 * The type of class with which other object resources must be instances of to be equal. This is a private variable to prevent interpretation as a property in
+	 * subclasses.
+	 */
+	private final Class<? extends C> type;
+
+	/**
+	 * URI constructor.
+	 * @param uri The URI for the new resource.
+	 */
+	public AbstractObjectResource(final URI uri, final Class<? extends C> type)
+	{
+		super(uri);
+		this.type = checkInstance(type);
+	}
+
+	/**
+	 * {@inheritDoc} This implementation compares the specified runtime type and resource URIs. If neither object has a reference URI, the default identity
+	 * comparison is performed.
+	 * @see #getType()
+	 * @see #getURI()
+	 */
+	public boolean equals(final Object object)
+	{
+		if(!type.isInstance(object)) //require the specified type
+		{
+			return false;
+		}
+		return super.equals(object);
+	}
+
+}
