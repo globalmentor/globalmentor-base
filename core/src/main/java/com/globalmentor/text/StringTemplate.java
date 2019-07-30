3c3e44bb5c49d882171c286591829806a941ea6f
Garret Wilson <garret@globalmentor.com>
2019-07-30 07:46:22 -0700

    JAVA-127: Moved string template class to text package.

--- core/src/main/java/com/globalmentor/text/StringTemplate.java
+++ core/src/main/java/com/globalmentor/text/StringTemplate.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 1996-2008 GlobalMentor, Inc. <http://www.globalmentor.com/>
+ * Copyright © 1996-2019 GlobalMentor, Inc. <http://www.globalmentor.com/>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.globalmentor.util;
+package com.globalmentor.text;
 
 import static java.util.Objects.*;
 
