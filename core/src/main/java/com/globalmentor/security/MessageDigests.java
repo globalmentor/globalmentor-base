914dedefb230ecb91f1c8c63f63ca579f52d65a8
Garret Wilson <garret@globalmentor.com>
2019-09-29 21:13:35 -0300

    JAVA-146: Introduced message digest algorithm identification type.
    The standard algorithm names were updated as of Java 13.

--- core/src/main/java/com/globalmentor/security/MessageDigests.java
+++ core/src/main/java/com/globalmentor/security/MessageDigests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 1996-2012 GlobalMentor, Inc. <http://www.globalmentor.com/>
+ * Copyright © 1996-2019 GlobalMentor, Inc. <http://www.globalmentor.com/>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,19 +19,80 @@ package com.globalmentor.security;
 import java.nio.charset.Charset;
 import java.security.*;
 
+import javax.annotation.*;
+
+import com.globalmentor.model.Named;
+
 import static com.globalmentor.java.Characters.*;
 import static java.nio.charset.StandardCharsets.*;
+import static java.util.Objects.*;
 
 /**
  * Utility methods for working with message digests.
+ * @apiNote The standard algorithms enumerated by this class identify known algorithms with standard names. Not all of them are guaranteed to be implemented on
+ *          any particular Java platform.
  * @author Garret Wilson
+ * @see <a href="https://docs.oracle.com/en/java/javase/11/docs/specs/security/standard-names.html#messagedigest-algorithms"><code>MessageDigest</code>
+ *      Algorithms</a>
  */
 public class MessageDigests {
 
-	/** The MD5 digest algorithm. */
-	public static final String MD5_ALGORITHM = "MD5";
-	/** The SHA digest algorithm. */
-	public static final String SHA_ALGORITHM = "SHA";
+	/** The MD2 message digest algorithm as defined in <a href="https://tools.ietf.org/html/rfc1319">RFC 1319</a>. */
+	public static final Algorithm MD2 = new Algorithm("MD2");
+
+	/**
+	 * The MD5 message digest algorithm as defined in <a href="https://tools.ietf.org/html/rfc1321">RFC 1321</a>. Guaranteed to be supported by every Java
+	 * platform implementation as of Java 7.
+	 */
+	public static final Algorithm MD5 = new Algorithm("MD5");
+
+	/**
+	 * A hash algorithms defined in <a href="https://csrc.nist.gov/publications/fips/fips180-4/fips-180-4.pdf">FIPS PUB 180-4<a>. Guaranteed to be supported by
+	 * every Java platform implementation as of Java 7.
+	 */
+	public static final Algorithm SHA_1 = new Algorithm("SHA-1");
+
+	/** A hash algorithms defined in <a href="https://csrc.nist.gov/publications/fips/fips180-4/fips-180-4.pdf">FIPS PUB 180-4<a>. */
+	public static final Algorithm SHA_224 = new Algorithm("SHA-224");
+
+	/**
+	 * A hash algorithms defined in <a href="https://csrc.nist.gov/publications/fips/fips180-4/fips-180-4.pdf">FIPS PUB 180-4<a>. Guaranteed to be supported by
+	 * every Java platform implementation as of Java 7.
+	 */
+	public static final Algorithm SHA_256 = new Algorithm("SHA-256");
+
+	/** A hash algorithms defined in <a href="https://csrc.nist.gov/publications/fips/fips180-4/fips-180-4.pdf">FIPS PUB 180-4<a>. */
+	public static final Algorithm SHA_384 = new Algorithm("SHA-384");
+
+	/** A hash algorithms defined in <a href="https://csrc.nist.gov/publications/fips/fips180-4/fips-180-4.pdf">FIPS PUB 180-4<a>. */
+	public static final Algorithm SHA_512_224 = new Algorithm("SHA-512/224");
+
+	/** A hash algorithms defined in <a href="https://csrc.nist.gov/publications/fips/fips180-4/fips-180-4.pdf">FIPS PUB 180-4<a>. */
+	public static final Algorithm SHA_512_256 = new Algorithm("SHA-512/256");
+
+	/**
+	 * Permutation-based hash and extendable-output functions as defined in <a href="https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.202.pdf">FIPS PUB 202</a>
+	 * producing a 224 bit digest.
+	 */
+	public static final Algorithm SHA3_224 = new Algorithm("SHA3-224");
+
+	/**
+	 * Permutation-based hash and extendable-output functions as defined in <a href="https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.202.pdf">FIPS PUB 202</a>
+	 * producing a 256 bit digest.
+	 */
+	public static final Algorithm SHA3_256 = new Algorithm("SHA3-256");
+
+	/**
+	 * Permutation-based hash and extendable-output functions as defined in <a href="https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.202.pdf">FIPS PUB 202</a>
+	 * producing a 384 bit digest.
+	 */
+	public static final Algorithm SHA3_384 = new Algorithm("SHA3-384");
+
+	/**
+	 * Permutation-based hash and extendable-output functions as defined in <a href="https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.202.pdf">FIPS PUB 202</a>
+	 * producing a 512 bit digest.
+	 */
+	public static final Algorithm SHA3_512 = new Algorithm("SHA3-512");
 
 	/**
 	 * Computes a digest for the given character sequences using the UTF-8 charset.
@@ -39,7 +100,7 @@ public class MessageDigests {
 	 * @param charSequences The character sequences to digest.
 	 * @return The array of bytes for the resulting hash value.
 	 */
-	public static byte[] digest(final MessageDigest messageDigest, final CharSequence... charSequences) {
+	public static byte[] digest(@Nonnull final MessageDigest messageDigest, @Nonnull final CharSequence... charSequences) {
 		return update(messageDigest, charSequences).digest(); //update the digest with the given character sequences and return the digest
 	}
 
@@ -50,7 +111,7 @@ public class MessageDigests {
 	 * @param charSequences The character sequences to digest.
 	 * @return The array of bytes for the resulting hash value.
 	 */
-	public static byte[] digest(final MessageDigest messageDigest, final Charset charset, final CharSequence... charSequences) {
+	public static byte[] digest(@Nonnull final MessageDigest messageDigest, @Nonnull final Charset charset, @Nonnull final CharSequence... charSequences) {
 		return update(messageDigest, charset, charSequences).digest(); //update the digest from the character sequence's characters using the given charset and return the digest
 	}
 
@@ -60,7 +121,7 @@ public class MessageDigests {
 	 * @param characters The characters to digest.
 	 * @return The array of bytes for the resulting hash value.
 	 */
-	public static byte[] digest(final MessageDigest messageDigest, final char[] characters) {
+	public static byte[] digest(@Nonnull final MessageDigest messageDigest, @Nonnull final char[] characters) {
 		return digest(messageDigest, UTF_8, characters); //digest the characters using UTF-8
 	}
 
@@ -71,7 +132,7 @@ public class MessageDigests {
 	 * @param characters The arrays of characters to digest.
 	 * @return The array of bytes for the resulting hash value.
 	 */
-	public static byte[] digest(final MessageDigest messageDigest, final Charset charset, final char[] characters) {
+	public static byte[] digest(@Nonnull final MessageDigest messageDigest, @Nonnull final Charset charset, @Nonnull final char[] characters) {
 		final byte[] bytes = toByteArray(characters, charset); //convert the characters to bytes
 		return messageDigest.digest(bytes); //calculate and return the digest
 	}
@@ -82,7 +143,7 @@ public class MessageDigests {
 	 * @param charSequences The character sequences to digest.
 	 * @return The message digest.
 	 */
-	public static MessageDigest update(final MessageDigest messageDigest, final CharSequence... charSequences) {
+	public static MessageDigest update(@Nonnull final MessageDigest messageDigest, @Nonnull final CharSequence... charSequences) {
 		return update(messageDigest, UTF_8, charSequences); //update the digest using UTF-8
 	}
 
@@ -93,7 +154,7 @@ public class MessageDigests {
 	 * @param charSequences The character sequences to digest.
 	 * @return The message digest.
 	 */
-	public static MessageDigest update(final MessageDigest messageDigest, final Charset charset, final CharSequence... charSequences) {
+	public static MessageDigest update(@Nonnull final MessageDigest messageDigest, @Nonnull final Charset charset, @Nonnull final CharSequence... charSequences) {
 		for(final CharSequence charSequence : charSequences) { //for each character sequence
 			update(messageDigest, charset, charSequence); //update the digest from the character sequence using the given charset
 		}
@@ -106,7 +167,7 @@ public class MessageDigests {
 	 * @param characters The characters to digest.
 	 * @return The message digest.
 	 */
-	public static MessageDigest update(final MessageDigest messageDigest, final char[] characters) {
+	public static MessageDigest update(@Nonnull final MessageDigest messageDigest, @Nonnull final char[] characters) {
 		return update(messageDigest, UTF_8, characters); //update the digest using UTF-8
 	}
 
@@ -117,7 +178,7 @@ public class MessageDigests {
 	 * @param charSequence The character sequence to digest.
 	 * @return The message digest.
 	 */
-	public static MessageDigest update(final MessageDigest messageDigest, final Charset charset, final CharSequence charSequence) {
+	public static MessageDigest update(@Nonnull final MessageDigest messageDigest, @Nonnull final Charset charset, @Nonnull final CharSequence charSequence) {
 		final byte[] bytes = charSequence.toString().getBytes(charset); //convert the characters to bytes
 		messageDigest.update(bytes); //update the digest
 		return messageDigest; //return the message digest
@@ -130,10 +191,132 @@ public class MessageDigests {
 	 * @param characters The arrays of characters to digest.
 	 * @return The message digest.
 	 */
-	public static MessageDigest update(final MessageDigest messageDigest, final Charset charset, final char[] characters) {
+	public static MessageDigest update(@Nonnull final MessageDigest messageDigest, @Nonnull final Charset charset, @Nonnull final char[] characters) {
 		final byte[] bytes = toByteArray(characters, charset); //convert the characters to bytes
 		messageDigest.update(bytes); //update the digest
 		return messageDigest; //return the message digest
+	}
+
+	/**
+	 * Encapsulation of a standard {@link MessageDigest} algorithm, which may or may not be implemented by any particular Java platform.
+	 * @author Garret Wilson
+	 * @see <a href="https://docs.oracle.com/en/java/javase/11/docs/specs/security/standard-names.html#messagedigest-algorithms"><code>MessageDigest</code>
+	 *      Algorithms</a>
+	 */
+	public final static class Algorithm implements Named<String> {
+
+		private final String name;
+
+		/**
+		 * Name constructor.
+		 * @param name The algorithm name.
+		 */
+		private Algorithm(@Nonnull final String name) {
+			this.name = requireNonNull(name);
+		}
+
+		@Override
+		public String getName() {
+			return name;
+		}
+
+		/**
+		 * Convenience method to get an instance of this message digest algorithm.
+		 * @apiNote This method differs from {@link MessageDigest#getInstance(String)} in that it throws an unchecked exception if the algorithm is not supported.
+		 * @return An instance of this message digest algorithm.
+		 * @throws RuntimeException if no {@link Provider} supports a {@link MessageDigestSpi} implementation for this algorithm.
+		 * @see MessageDigest#getInstance(String)
+		 */
+		public MessageDigest getInstance() {
+			try {
+				return MessageDigest.getInstance(getName());
+			} catch(final NoSuchAlgorithmException noSuchAlgorithmException) {
+				throw new RuntimeException(noSuchAlgorithmException);
+			}
+		}
+
+		/**
+		 * Computes a digest using this algorithm for the given character sequences using the UTF-8 charset.
+		 * @implSpec This convenience method delegates to {@link MessageDigests#digest(MessageDigest, CharSequence...)}.
+		 * @param charSequences The character sequences to digest.
+		 * @return The array of bytes for the resulting hash value.
+		 * @throws RuntimeException if no {@link Provider} supports a {@link MessageDigestSpi} implementation for this algorithm.
+		 */
+		public byte[] digest(@Nonnull final CharSequence... charSequences) {
+			return MessageDigests.digest(getInstance(), charSequences);
+		}
+
+		/**
+		 * Computes a digest using this algorithm for the given character sequences, using the given charset.
+		 * @implSpec This convenience method delegates to {@link MessageDigests#digest(MessageDigest, Charset, CharSequence...)}.
+		 * @param charset The charset to use when converting characters to bytes.
+		 * @param charSequences The character sequences to digest.
+		 * @return The array of bytes for the resulting hash value.
+		 * @throws RuntimeException if no {@link Provider} supports a {@link MessageDigestSpi} implementation for this algorithm.
+		 */
+		public byte[] digest(@Nonnull final Charset charset, @Nonnull final CharSequence... charSequences) {
+			return MessageDigests.digest(getInstance(), charSequences);
+		}
+
+		/**
+		 * Computes a digest using this algorithm for the given characters using the UTF-8 charset.
+		 * @implSpec This convenience method delegates to {@link MessageDigests#digest(MessageDigest, char[])}.
+		 * @param characters The characters to digest.
+		 * @return The array of bytes for the resulting hash value.
+		 * @throws RuntimeException if no {@link Provider} supports a {@link MessageDigestSpi} implementation for this algorithm.
+		 */
+		public byte[] digest(@Nonnull final char[] characters) {
+			return MessageDigests.digest(getInstance(), characters);
+		}
+
+		/**
+		 * Computes a digest using this algorithm for the given characters, using the given charset.
+		 * @implSpec This convenience method delegates to {@link MessageDigests#digest(MessageDigest, Charset, char[])}.
+		 * @param charset The charset to use when converting characters to bytes.
+		 * @param characters The arrays of characters to digest.
+		 * @return The array of bytes for the resulting hash value.
+		 * @throws RuntimeException if no {@link Provider} supports a {@link MessageDigestSpi} implementation for this algorithm.
+		 */
+		public byte[] digest(@Nonnull final Charset charset, @Nonnull final char[] characters) {
+			return MessageDigests.digest(getInstance(), charset, characters);
+		}
+
+		/**
+		 * {@inheritDoc}
+		 * @implSpec This implementation returns the hash code of the algorithm name.
+		 * @see #getName()
+		 */
+		@Override
+		public int hashCode() {
+			return name.hashCode();
+		}
+
+		/**
+		 * {@inheritDoc}
+		 * @implSpec This implementation considers two algorithms equal if they have the same name.
+		 * @see #getName()
+		 */
+		@Override
+		public boolean equals(@Nonnull final Object object) {
+			if(this == object) {
+				return true;
+			}
+			if(!(object instanceof Algorithm)) {
+				return false;
+			}
+			return getName().equals(((Algorithm)object).getName());
+		}
+
+		/**
+		 * {@inheritDoc}
+		 * @implSpec This version return the algorithm name.
+		 * @see #getName()
+		 */
+		@Override
+		public String toString() {
+			return getName();
+		}
+
 	}
 
 }
