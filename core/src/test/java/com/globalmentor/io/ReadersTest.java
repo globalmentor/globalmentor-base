e0a297b821523630d32e7a05184f1fd33d590587
Garret Wilson <garret@globalmentor.com>
2020-06-06 15:02:41 -0700

    JAVA-177: Added Reader methods that guarantee data returned if not end.

--- core/src/test/java/com/globalmentor/io/ReadersTest.java
+++ core/src/test/java/com/globalmentor/io/ReadersTest.java
@@ -16,6 +16,8 @@
 
 package com.globalmentor.io;
 
+import static com.globalmentor.io.Readers.*;
+import static com.globalmentor.java.Characters.*;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -29,6 +31,88 @@ import org.junit.jupiter.api.Test;
  * @author Garret Wilson
  */
 public class ReadersTest {
+
+	/** @see Readers#read(Reader, char[]) */
+	@Test
+	public void testRead() throws IOException {
+		final Reader reader = new StringReader("foobar");
+		final char[] buffer = new char[6]; //initializes to all zeros
+		assertThat(Readers.read(reader, buffer), is(6)); //correct number of bytes read
+		assertThat(buffer, is(new char[] {'f', 'o', 'o', 'b', 'a', 'r'})); //correct data read
+		assertThat(readString(reader), is("")); //no data remaining
+	}
+
+	/** @see Readers#read(Reader, char[]) */
+	@Test
+	public void testReadEmptyReader() throws IOException {
+		final Reader reader = new StringReader("");
+		final char[] buffer = new char[6]; //initializes to all zeros
+		assertThat(Readers.read(reader, buffer), is(0)); //correct number of bytes read
+		assertThat(buffer, is(new char[] {0, 0, 0, 0, 0, 0})); //correct data read
+		assertThat(readString(reader), is("")); //no data remaining
+	}
+
+	/** @see Readers#read(Reader, char[]) */
+	@Test
+	public void testReadEmptyBuffer() throws IOException {
+		final Reader reader = new StringReader("foobar");
+		final char[] buffer = new char[0]; //initializes to all zeros
+		assertThat(Readers.read(reader, buffer), is(0)); //correct number of bytes read
+		assertThat(buffer, is(NO_CHARS)); //no data read
+		assertThat(readString(reader), is("foobar")); //correct data remaining
+	}
+
+	/** @see Readers#read(Reader, char[], int) */
+	@Test
+	public void testReadLength() throws IOException {
+		final Reader reader = new StringReader("foobar");
+		final char[] buffer = new char[8]; //initializes to all zeros
+		assertThat(Readers.read(reader, buffer, 3), is(3)); //correct number of bytes read
+		assertThat(buffer, is(new char[] {'f', 'o', 'o', 0, 0, 0, 0, 0})); //correct data read
+		assertThat(readString(reader), is("bar")); //correct data still remaining
+	}
+
+	/** @see Readers#read(Reader, char[], int) */
+	@Test
+	public void testReadNegativeLengthThrowsException() throws IOException {
+		final Reader reader = new StringReader("foobar");
+		final char[] buffer = new char[8]; //initializes to all zeros
+		assertThrows(IndexOutOfBoundsException.class, () -> Readers.read(reader, buffer, -1));
+		assertThrows(IndexOutOfBoundsException.class, () -> Readers.read(reader, buffer, -2));
+		assertThrows(IndexOutOfBoundsException.class, () -> Readers.read(reader, buffer, -123));
+		assertThat(buffer, is(new char[] {0, 0, 0, 0, 0, 0, 0, 0})); //correct data read
+		assertThat(readString(reader), is("foobar")); //correct data still remaining
+	}
+
+	/** @see Readers#read(Reader, char[], int, int) */
+	@Test
+	public void testReadWithRemainingData() throws IOException {
+		final Reader reader = new StringReader("extraordinary");
+		final char[] buffer = new char[10]; //initializes to all zeros
+		assertThat(Readers.read(reader, buffer, 2, 5), is(5)); //correct number of bytes read
+		assertThat(buffer, is(new char[] {0, 0, 'e', 'x', 't', 'r', 'a', 0, 0, 0})); //correct data read
+		assertThat(readString(reader), is("ordinary")); //correct data still remaining
+	}
+
+	/** @see Readers#read(Reader, char[], int, int) */
+	@Test
+	public void testReadBufferExactLengthRequested() throws IOException {
+		final Reader reader = new StringReader("foobar");
+		final char[] buffer = new char[8]; //initializes to all zeros
+		assertThat(Readers.read(reader, buffer, 1, 6), is(6)); //correct number of bytes read
+		assertThat(buffer, is(new char[] {0, 'f', 'o', 'o', 'b', 'a', 'r', 0})); //correct data read
+		assertThat(readString(reader), is("")); //no data remaining
+	}
+
+	/** @see Readers#read(Reader, char[], int, int) */
+	@Test
+	public void testReadRemainingBuffer() throws IOException {
+		final Reader reader = new StringReader("foo");
+		final char[] buffer = new char[8]; //initializes to all zeros
+		assertThat(Readers.read(reader, buffer, 1, 3), is(3)); //correct number of bytes read
+		assertThat(buffer, is(new char[] {0, 'f', 'o', 'o', 0, 0, 0, 0})); //correct data read
+		assertThat(readString(reader), is("")); //no data remaining
+	}
 
 	/** @see Readers#readString(Reader), int) */
 	@Test
