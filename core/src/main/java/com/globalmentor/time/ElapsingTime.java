0254cd357e0c928e09c587935beafc74716142f1
Garret Wilson <garret@globalmentor.com>
2023-07-18 21:00:58 -0300

    JAVA-314: Added elapsing time wither methods for use by recurring time.

--- core/src/main/java/com/globalmentor/time/ElapsingTime.java
+++ core/src/main/java/com/globalmentor/time/ElapsingTime.java
@@ -20,6 +20,7 @@ import static java.util.Objects.*;
 import static java.util.concurrent.TimeUnit.*;
 
 import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.concurrent.TimeUnit;
 import java.util.function.*;
 
@@ -31,6 +32,8 @@ import javax.annotation.*;
  *          replace boilerplate and reduce the risk of errors through repetition; it is not intended to compete with more full-function classes that allow
  *          starting and stopping counting, or provide new pluggable composite types for mocking time sources (although this class does use a general
  *          {@link Supplier} of time).
+ * @implSpec This implementation considers two instances equal if they have the same start time and time unit, without regard to the time supplier, which is
+ *           considered an implementation detail.
  * @implSpec By default this class uses {@link System#nanoTime()} as its time supplier, but any time supplier can be used with any {@link TimeUnit}.
  * @author Garret Wilson
  * @see <a href="https://stackoverflow.com/q/180158">How do I time a method's execution in Java?</a>
@@ -46,6 +49,11 @@ public final class ElapsingTime {
 	private final LongSupplier timeSupplier;
 
 	private final TimeUnit timeUnit;
+
+	/** @return The time unit this instant is using to track time. */
+	TimeUnit getTimeUnit() {
+		return timeUnit;
+	}
 
 	/**
 	 * Start time, supplier, and unit constructor.
@@ -63,7 +71,7 @@ public final class ElapsingTime {
 	 * Creates a means of measuring elapsed time since the given time.
 	 * @implSpec This method uses {@link System#nanoTime()} as the source of nanosecond time.
 	 * @param startNanoTime The start time in nanoseconds, in terms of {@link System#nanoTime()}.
-	 * @return A new elapsing time measurer.
+	 * @return A new elapsing time measurer elapsing since the given start time.
 	 * @see #sinceNow()
 	 */
 	public static ElapsingTime sinceNanoTime(final long startNanoTime) {
@@ -75,7 +83,7 @@ public final class ElapsingTime {
 	 * @param startTime The start time in the given unit and in terms of the time supplier.
 	 * @param timeSupplier The time source for measuring elapsed time since the start time.
 	 * @param timeUnit The unit in which time is measured.
-	 * @return A new elapsing time measurer.
+	 * @return A new elapsing time measurer elapsing since the given start time.
 	 * @see #sinceNow(LongSupplier, TimeUnit)
 	 */
 	public static ElapsingTime since(final long startTime, @Nonnull final LongSupplier timeSupplier, @Nonnull final TimeUnit timeUnit) {
@@ -96,10 +104,35 @@ public final class ElapsingTime {
 	 * Creates a means of measuring elapsed time from the current time.
 	 * @apiNote This method is analogous to {@link #sinceNanoTime(long)}, and is named "from" because "since" typically designates a starting time in the past.
 	 * @implSpec This method uses {@link System#nanoTime()} as the source of nanosecond time.
-	 * @return A new elapsing time measurer.
+	 * @return A new elapsing time measurer elapsing from now.
 	 */
 	public static ElapsingTime fromNow() {
 		return fromNow(System::nanoTime, NANOSECONDS);
+	}
+
+	/**
+	 * Copy static factory "wither" method that creates a new elapsing time from the current time, using the same time supplier and time unit as this elapsing
+	 * time instance.
+	 * @apiNote This method is equivalent to calling {@link #withElapsed(Duration)} with {@link Duration#ZERO}.
+	 * @implSpec This implementation delegates to {@link #fromNow(LongSupplier, TimeUnit)}.
+	 * @return A new elapsing time measurer elapsing from now.
+	 */
+	public ElapsingTime withElapsingFromNow() {
+		return fromNow(timeSupplier, getTimeUnit());
+	}
+
+	/**
+	 * Copy static factory "wither" method that creates a new elapsing time from the current time plus or minus the given duration, using the same time supplier
+	 * and time unit as this elapsing time instance.
+	 * @apiNote The provided duration can be positive, negative, or even {@link Duration#ZERO}. A positive duration indicates that a the resulting elapsed time
+	 *          <em>has already started elapsing in the past</em>, at a time equal to now <em>minus</em> the given positive duration. A negative duration produces
+	 *          an elapsing time that has not yet started elapsing, and which will produce negative elapsing time durations until the given duration has passed.
+	 * @implSpec This implementation delegates to {@link #since(long, LongSupplier, TimeUnit)}.
+	 * @param duration The duration of time the resulting elapsing time will conceptually have already have been elapsing when returned.
+	 * @return A new elapsing time measurer that conceptually has already been elapsing since the given duration.
+	 */
+	public ElapsingTime withElapsed(@Nonnull final Duration duration) { //TODO write unit tests
+		return since(timeSupplier.getAsLong() - duration.get(toChronoUnit(getTimeUnit())), timeSupplier, getTimeUnit());
 	}
 
 	/**
@@ -138,12 +171,62 @@ public final class ElapsingTime {
 	 * @return The amount of time elapsed until the current time in the requested unit.
 	 */
 	public long get(@Nonnull final TimeUnit asTimeUnit) {
-		return asTimeUnit.convert(getElapsedTime(), timeUnit);
+		return asTimeUnit.convert(getElapsedTime(), getTimeUnit());
 	}
 
 	/** @return The time elapsed until the current time. */
 	public Duration get() {
-		return Duration.ofNanos(get(NANOSECONDS)); //TODO change to `Duration.of(getElapsedTime(), timeUnit.toChronoUnit())` in Java 9+
+		return Duration.of(getElapsedTime(), toChronoUnit(getTimeUnit()));
+	}
+
+	@Override
+	public int hashCode() {
+		return hash(startTime, timeUnit);
+	}
+
+	@Override
+	public boolean equals(final Object object) {
+		if(this == object) {
+			return true;
+		}
+		if(!(object instanceof ElapsingTime)) {
+			return false;
+		}
+		final ElapsingTime elapsingTime = (ElapsingTime)object;
+		return startTime == elapsingTime.startTime && getTimeUnit().equals(elapsingTime.getTimeUnit());
+	}
+
+	@Override
+	public String toString() {
+		return "…" + get().toString() + "…";
+	}
+
+	/**
+	 * Converts a {@link TimeUnit} to the equivalent {@link ChronoUnit}.
+	 * @apiNote This method is equivalent to the similarly-named {@link TimeUnit} method introduced in Java 9. This method will be deprecated and removed once
+	 *          this library is upgraded to require Java 9+.
+	 * @param timeUnit Some time unit.
+	 * @return The converted {@link ChronoUnit} equivalent to the given {@link TimeUnit}.
+	 */
+	static ChronoUnit toChronoUnit(@Nonnull final TimeUnit timeUnit) { //TODO switch to `timeUnit.toChronoUnit())` in Java 9+
+		switch(timeUnit) {
+			case NANOSECONDS:
+				return ChronoUnit.NANOS;
+			case MICROSECONDS:
+				return ChronoUnit.MICROS;
+			case MILLISECONDS:
+				return ChronoUnit.MILLIS;
+			case SECONDS:
+				return ChronoUnit.SECONDS;
+			case MINUTES:
+				return ChronoUnit.MINUTES;
+			case HOURS:
+				return ChronoUnit.HOURS;
+			case DAYS:
+				return ChronoUnit.DAYS;
+			default:
+				throw new AssertionError();
+		}
 	}
 
 }
