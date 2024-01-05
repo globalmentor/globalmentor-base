8e09c6671ad22a89fdf07a184e15767ad15c95b4
Garret Wilson <garret@globalmentor.com>
2024-01-05 10:59:45 -0300

    JAVA-75: Added UUID conversion utilities to and from bytes.

--- core/src/main/java/com/globalmentor/model/UUIDs.java
+++ core/src/main/java/com/globalmentor/model/UUIDs.java
@@ -19,6 +19,11 @@ package com.globalmentor.model;
 import java.net.URI;
 import java.util.UUID;
 
+import javax.annotation.*;
+
+import com.globalmentor.java.Longs;
+
+import static com.globalmentor.java.Conditions.*;
 import static com.globalmentor.java.StringBuilders.*;
 import static com.globalmentor.net.URIs.*;
 
@@ -31,15 +36,90 @@ public final class UUIDs {
 	private UUIDs() {
 	}
 
+	/** The number of bytes in a UUID. */
+	public static final int BYTE_COUNT = 16;
+
 	/** The UUID URN namespace identifier "uuid". */
 	public static final String UUID_URN_NAMESPACE = "uuid";
+
+	/**
+	 * Creates a UUID from its raw binary representation in bytes.
+	 * @apiNote This method requires exactly the number of bytes necessary. If some of the bytes should be ignored, use {@link #fromBytes(byte[], int)}.
+	 * @implSpec This implementation delegates to {@link #fromBytes(byte[], int)}.
+	 * @param bytes The bits of the UUID stored in big-endian order (most significant bits first).
+	 * @return A UUID instance equivalent to the raw binary representation.
+	 * @see UUID#getMostSignificantBits()
+	 * @see UUID#getLeastSignificantBits()
+	 */
+	public static UUID fromBytes(@Nonnull final byte[] bytes) {
+		checkArgument(bytes.length == BYTE_COUNT, "Exactly %d bytes are required to define a UUID.", BYTE_COUNT);
+		return fromBytes(bytes, 0);
+	}
+
+	/**
+	 * Creates a UUID from its raw binary representation in bytes.
+	 * @param bytes The bits of the UUID stored in big-endian order (most significant bits first).
+	 * @param index The initial index in the array at which the bytes are stored.
+	 * @return A UUID instance equivalent to the raw binary representation.
+	 * @see UUID#getMostSignificantBits()
+	 * @see UUID#getLeastSignificantBits()
+	 */
+	public static UUID fromBytes(@Nonnull final byte[] bytes, final int index) {
+		return new UUID(Longs.fromBytes(bytes, index), Longs.fromBytes(bytes, index + Long.BYTES));
+	}
+
+	/**
+	 * Converts a UUID to its equivalent big-endian sequence of bytes.
+	 * @implSpec This implementation delegates to {@link #toBytes(UUID, byte[])}.
+	 * @param uuid The UUID to convert to bytes.
+	 * @return The bytes equivalent to the UUID value, most-significant bytes first.
+	 * @throws ArrayIndexOutOfBoundsException if the given array does not have sufficient bytes to store the UUID value.
+	 * @see UUID#getMostSignificantBits()
+	 * @see UUID#getLeastSignificantBits()
+	 * @see #fromBytes(byte[])
+	 */
+	public static byte[] toBytes(@Nonnull final UUID uuid) {
+		return toBytes(uuid, new byte[BYTE_COUNT]);
+	}
+
+	/**
+	 * Converts a UUID to its equivalent big-endian sequence of bytes, storing the bytes in the given array.
+	 * @implSpec This implementation delegates to {@link #toBytes(UUID, byte[], int)}.
+	 * @param uuid The UUID to convert to bytes.
+	 * @param bytes The array in which to store the bytes.
+	 * @return The given array of bytes with the UUID stored.
+	 * @throws ArrayIndexOutOfBoundsException if the given array does not have sufficient bytes to store the UUID value.
+	 * @see UUID#getMostSignificantBits()
+	 * @see UUID#getLeastSignificantBits()
+	 * @see #fromBytes(byte[])
+	 */
+	public static byte[] toBytes(@Nonnull final UUID uuid, @Nonnull final byte[] bytes) {
+		return toBytes(uuid, bytes, 0);
+	}
+
+	/**
+	 * Converts a UUID to its equivalent big-endian sequence of bytes, storing the bytes in the given array at the given index.
+	 * @param uuid The UUID to convert to bytes.
+	 * @param bytes The array in which to store the bytes.
+	 * @param index The index in the array at which to begin storing the bytes.
+	 * @return The given array of bytes with the UUID stored.
+	 * @throws ArrayIndexOutOfBoundsException if the given array, starting at the given index, does not have sufficient bytes to store the UUID value.
+	 * @see UUID#getMostSignificantBits()
+	 * @see UUID#getLeastSignificantBits()
+	 * @see #fromBytes(byte[])
+	 */
+	public static byte[] toBytes(@Nonnull final UUID uuid, @Nonnull final byte[] bytes, final int index) {
+		Longs.toBytes(uuid.getMostSignificantBits(), bytes, index);
+		Longs.toBytes(uuid.getLeastSignificantBits(), bytes, index + Long.BYTES);
+		return bytes;
+	}
 
 	/**
 	 * Constructs a string of hexadecimal characters equivalent to the return value of {@link UUID#toString()} with all non-digits removed.
 	 * @param uuid The UUID from which to construct a hex string.
 	 * @return A pure hex string representing the UUID.
 	 */
-	public static String toHexString(final UUID uuid) {
+	public static String toHexString(@Nonnull final UUID uuid) {
 		final StringBuilder hexStringBuilder = new StringBuilder(uuid.toString()); //create a string from the UUID
 		return removeEvery(hexStringBuilder, '-').toString(); //remove all the hyphens and return the resulting string
 	}
@@ -50,7 +130,7 @@ public final class UUIDs {
 	 * @param uuid The UUID.
 	 * @return A URI representing the UUID.
 	 */
-	public static URI toURI(final UUID uuid) {
+	public static URI toURI(@Nonnull final UUID uuid) {
 		return createURN(UUID_URN_NAMESPACE, uuid.toString()); //construct an return the URN
 	}
 
