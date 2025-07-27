834048de6f35f5cacf526993a4e1bb85f45882de
Garret Wilson <garret@globalmentor.com>
2025-07-27 08:11:30 -0700

    JAVA-409: Converted from using legacy `File` to Java NIO `Path`.

--- text-csv/src/main/java/com/globalmentor/text/csv/CSV.java
+++ text-csv/src/main/java/com/globalmentor/text/csv/CSV.java
@@ -17,17 +17,21 @@
 package com.globalmentor.text.csv;
 
 import java.io.*;
+import java.nio.file.Path;
 
 import com.globalmentor.java.Characters;
 import com.globalmentor.net.MediaType;
 
 import static com.globalmentor.java.Characters.*;
 import static java.nio.charset.StandardCharsets.*;
+import static java.nio.file.Files.*;
+import static java.nio.file.StandardOpenOption.*;
 
 /**
  * Constant values for Comma Separated Value (CSV) files.
  * @author Garret Wilson
- * @see <a href="http://www.ietf.org/rfc/rfc4180.txt">RFC 4180: Common Format and MIME Type for Comma-Separated Values (CSV) Files</a>
+ * @see <a href="https://www.rfc-editor.org/rfc/rfc4180.html">RFC 4180: Common Format and MIME Type for Comma-Separated Values (CSV) Files</a>
+ * @see <a href="https://www.rfc-editor.org/rfc/inline-errata/rfc4180.html">RFC 4180 with errata</a>
  */
 public final class CSV {
 
@@ -56,33 +60,39 @@ public final class CSV {
 	public static final String ESCAPED_QUOTATION_MARK_STRING = "" + QUOTATION_MARK_CHAR + QUOTATION_MARK_CHAR;
 
 	/**
-	 * Appends a record to the given file with no headers. The information is encoded using UTF-8.
-	 * @param file The CSV file.
+	 * Appends a record to the file at the given path with no headers. The information is encoded using UTF-8.
+	 * @param path The path to the CSV file.
 	 * @param objects The objects to serialize.
 	 * @throws IOException if there is an error writing to the file.
 	 */
-	public static void appendRecord(final File file, final Object[] objects) throws IOException {
-		appendRecord(file, null, objects); //write the objects to the file without headers
+	public static void appendRecord(final Path path, final Object... objects) throws IOException {
+		appendRecord(path, null, objects); //write the objects to the file without headers
 	}
 
 	/**
-	 * Appends a record to the given file, first adding the provided headers (if any) if the file does not yet exist. The information is encoded using UTF-8.
-	 * @param file The CSV file.
+	 * Appends a record to the file at the given path, first adding the provided headers (if any) if the file does not yet exist. The information is encoded using
+	 * UTF-8.
+	 * @implNote This implementation has a small race condition in which the header could be written twice (and not necessarily at the beginning of the file) if
+	 *           multiple threads are appending to the file, and the file does not exist when this method is invoked while another thread creates the file and
+	 *           appends at the same time.
+	 * @param path The path to the CSV file.
 	 * @param headers The headers with which to initialize the file, or <code>null</code> if no headers are desired.
 	 * @param objects The objects to serialize, each of which may be <code>null</code>, or <code>null</code> if there are no objects to serialize.
 	 * @throws IOException if there is an error writing to the file.
 	 */
-	public static void appendRecord(final File file, final String[] headers, final Object[] objects) throws IOException {
-		final boolean exists = file.exists(); //see if the file exists
-		//create a writer to the file, encoding in UTF-8
-		try (final BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file, true), UTF_8))) {
+	public static void appendRecord(final Path path, final String[] headers, final Object... objects) throws IOException {
+		final boolean exists = exists(path);
+		//create a writer to the file, encoding in UTF-8; append to the an existing file, creating one if needed
+		try (final OutputStream outputStream = newOutputStream(path, CREATE, APPEND);
+				final BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
+				final Writer writer = new OutputStreamWriter(bufferedOutputStream, UTF_8)) {
 			if(!exists && headers != null) { //if the file didn't exist and there headers
-				CsvSerializer.serialize(bufferedWriter, (Object[])headers); //write the headers
+				CsvSerializer.serialize(writer, (Object[])headers); //write the headers
 			}
 			if(objects != null) { //if there are objects to serialize
-				CsvSerializer.serialize(bufferedWriter, objects); //write the objects
+				CsvSerializer.serialize(writer, objects); //write the objects
 			}
-			bufferedWriter.flush(); //flush everything we've written
+			writer.flush(); //flush everything we've written
 		}
 	}
 }
