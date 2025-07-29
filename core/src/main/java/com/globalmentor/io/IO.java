75f902d01fb03641eddc6c12864b5bc382f2e079
Garret Wilson <garret@globalmentor.com>
2025-07-29 16:52:10 -0700

    JAVA-410: Added I/O check methods.

--- core/src/main/java/com/globalmentor/io/IO.java
+++ core/src/main/java/com/globalmentor/io/IO.java
@@ -32,6 +32,36 @@ public final class IO {
 	}
 
 	/**
+	 * Checks the result of an expression and throws an {@link IOException} if the value is <code>false</code>.
+	 * @param test The result of the test.
+	 * @throws IOException if the given value is <code>false</code>.
+	 */
+	public static void check(final boolean test) throws IOException {
+		check(test, null); //check the test with no description
+	}
+
+	/**
+	 * Checks the result of an expression and throws an {@link IOException} if the value is <code>false</code>.
+	 * @param test The result of the test.
+	 * @param description A description of the test to be used when generating an exception, optionally formatted with arguments, or <code>null</code> for no
+	 *          description.
+	 * @param arguments The arguments to be applied when formatting, or an empty array if the message should not be formatted.
+	 * @throws NullPointerException if the given arguments is <code>null</code>.
+	 * @throws IOException if the given value is <code>false</code>.
+	 * @throws IllegalArgumentException if the description is an invalid pattern, or if an argument in the arguments array is not of the type expected by the
+	 *           format element(s) that use it.
+	 * @see String#format(String, Object...)
+	 */
+	public static void check(final boolean test, String description, final Object... arguments) throws IOException {
+		if(!test) { //format the message if appropriate
+			if(description != null && arguments.length > 0) {
+				description = String.format(description, arguments);
+			}
+			throw new IOException(description);
+		}
+	}
+
+	/**
 	 * Converts an object to a {@link Closeable} instance so that it can be used with try-with-resources.
 	 * @apiNote A more general auto-closeable interface would be {@link AutoCloseable}, although that interface throws more generalized exceptions; a similar
 	 *          method for {@link AutoCloseable} is available at {@link com.globalmentor.java.Objects#toAutoCloseable(Object)}.
